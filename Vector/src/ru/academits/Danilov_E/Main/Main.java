f04d8f60d2a751f5e6d2135e5657752b00252f3e
Jonman <Jonman@yandex.ru>
2024-02-04 17:55:12 +0700

    Task Vector Task 1 done

--- Vector/src/ru/academits/Danilov_E/Main/Main.java
+++ Vector/src/ru/academits/Danilov_E/Main/Main.java
@@ -1,6 +1,28 @@
 package ru.academits.Danilov_E.Main;
+
+import ru.academits.Danilov_E.Vector.Vector;
+
 public class Main {
     public static void main(String[] args) {
+        int n = 2;
+        double[] array = new double[]{3.4, 3.3, 5.7, 6.7};
+        double[] array1 = new double[]{2.4, 1.3, 0.7, 3.7};
 
+        Vector vector = new Vector(4);
+        Vector vector2 = new Vector(array);
+        Vector vector1 = new Vector(array1);
+        Vector vector3 = new Vector(n, array);
+
+        for (int i = 0; i < vector.getComponentsArray().length; i++) {
+            System.out.println(vector.getComponentsArray()[i]);
+        }
+
+        String string = vector2.toString();
+
+        System.out.println(string);
+
+        vector2.makeSubtraction(vector1);
+        string = vector2.toString();
+        System.out.println(string);
     }
 }
--- Vector/src/ru/academits/Danilov_E/Vector/Vector.java
+++ Vector/src/ru/academits/Danilov_E/Vector/Vector.java
@@ -1,4 +1,99 @@
 package ru.academits.Danilov_E.Vector;
 
+import java.util.Arrays;
+
 public class Vector {
+    private double[] componentsValues;
+    private int n;
+
+    public Vector(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n must be > 0");
+        }
+
+        this.n = n;
+        componentsValues = new double[n];
+        Arrays.fill(componentsValues, 0);
+    }
+
+    public Vector(Vector vector) {
+        this.n = vector.n;
+        this.componentsValues = new double[vector.n];
+
+        System.arraycopy(vector.componentsValues, 0, this.componentsValues, 0, vector.getComponentsArray().length);
+    }
+
+    public Vector(double[] array) {
+        this.n = array.length;
+        componentsValues = new double[array.length];
+        System.arraycopy(array, 0, componentsValues, 0, array.length);
+    }
+
+    public Vector(int n, double[] array) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n must be > 0");
+        }
+
+        this.n = n;
+        componentsValues = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            if (i < array.length) {
+                componentsValues[i] = array[i];
+                continue;
+            }
+
+            componentsValues[i] = 0;
+        }
+    }
+
+    public int getSize() {
+        return n;
+    }
+
+    public double[] getComponentsArray() {
+        return componentsValues;
+    }
+
+    public String toString() { // Изначально делал без StringBuilder, но IDEA рекомендовала его использовать
+        StringBuilder stringBuilder = new StringBuilder("{");
+
+        for (int i = 0; i < componentsValues.length; i++) {
+            stringBuilder.append(componentsValues[i]);
+
+            if (i != componentsValues.length - 1) {
+                stringBuilder.append(", ");
+            }
+        }
+
+        stringBuilder.append("}");
+
+        return String.valueOf(stringBuilder);
+    }
+
+    public void makeAddition(Vector vector){
+        for(int i = 0; i < Math.min(this.componentsValues.length, vector.componentsValues.length); i++){
+            this.componentsValues[i] = this.componentsValues[i] + vector.componentsValues[i];
+        }
+    }
+
+    public void makeSubtraction(Vector vector){
+        for(int i = 0; i < Math.min(this.componentsValues.length, vector.componentsValues.length); i++){
+            this.componentsValues[i] = this.componentsValues[i] - vector.componentsValues[i];
+        }
+    }
+
+    public void makeMultiplication(double number){
+        for(int i = 0; i < componentsValues.length; i++){
+            componentsValues[i] = componentsValues[i] * number;
+        }
+    }
+
+    public void makeReverse(){
+        for(int i = 0; i < componentsValues.length; i++){
+            componentsValues[i] = componentsValues[i] * -1;
+        }
+    }
+
+
 }
