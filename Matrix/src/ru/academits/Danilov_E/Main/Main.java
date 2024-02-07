caf77770159cd0a4247c54bf39aba2cb29e42a51
Jonman <Jonman@yandex.ru>
2024-02-08 00:27:37 +0700

    Task Matrix part 1 done

--- Matrix/src/ru/academits/Danilov_E/Main/Main.java
+++ Matrix/src/ru/academits/Danilov_E/Main/Main.java
@@ -1,7 +1,56 @@
 package ru.academits.Danilov_E.Main;
 
+import ru.academits.Danilov_E.Matrix.Matrix;
+import ru.academits.Danilov_E.Vector.Vector;
+
 public class Main {
     public static void main(String[] args) {
+        double[] array1 = new double[]{-3.4, 3.3, 5.7, 6.7};
+        double[] array2 = new double[]{2.4, 1.3, -0.7, 3.7, 1.1, 5.6};
+        double[] array3 = new double[]{6.7, -3.1, 0.0, 8.8, 2.7, 9.9, 4.2, 5.5};
 
+        double[][] myArray = {{1.1, 2.2, 3.3}, {4.4, 5.5, 6.6}, {7.7, 8.8}};
+        Matrix matrix = new Matrix(4, 8);
+        matrix.print();
+        Matrix matrix1 = new Matrix(matrix);
+        System.out.println();
+        matrix1.print();
+        Matrix matrix2 = new Matrix(myArray);
+        System.out.println();
+        matrix2.print();
+
+        Vector vector1 = new Vector(array1);
+        Vector vector2 = new Vector(array2);
+        Vector vector3 = new Vector(array3);
+
+        Vector[] vectors = {vector1, vector2, vector3};
+
+        Matrix matrix3 = new Matrix(vectors);
+        System.out.println();
+        matrix3.print();
+        System.out.println();
+
+        System.out.println(matrix3.getVector(1));
+        System.out.println();
+
+        matrix3.setVector(1, new Vector(8));
+        matrix3.print();
+        System.out.println();
+
+        System.out.println(matrix3.getColumnVector(0));
+        System.out.println();
+
+        matrix3.transposition();
+        matrix3.print();
+        System.out.println();
+
+        matrix2.print();
+        System.out.println();
+        matrix2.makeMultiplication(2);
+        matrix2.print();
+        System.out.println();
+
+        System.out.println(matrix2);
+        System.out.println();
     }
 }
--- Matrix/src/ru/academits/Danilov_E/Matrix/Matrix.java
+++ Matrix/src/ru/academits/Danilov_E/Matrix/Matrix.java
@@ -2,7 +2,142 @@ package ru.academits.Danilov_E.Matrix;
 
 import ru.academits.Danilov_E.Vector.Vector;
 
-public class Matrix {
+public class Matrix extends Vector{
+    private int n;
+    private int m;
+    Vector[] vectors;
+    public Matrix(int n, int m){
+        super(m); // Не совсем понимаю почему он требует задать родительский класс, ведь я внизу жестко указал конструктор для Vector
+        vectors = new Vector[n];
+        this.n = n;
+        this.m = m;
 
+        for(int i = 0; i < n; i++){
+            vectors[i] = new Vector(m);
+        }
+    }
+
+    public Matrix(Matrix matrix){
+        super(matrix.m);
+        this.n = matrix.n;
+        this.m = matrix.m;
+        this.vectors = matrix.vectors;
+    }
+
+    public Matrix(double[][] doubleArray){
+        super(doubleArray[0].length, doubleArray[0]); // Пытался подобрать конструктор отличный от super(m),
+        // но смущает, что передается только 0-й элемент. Данный конструктор выбрал ввиду возможного
+        // разного количества элементов внутри объектов Vector
+        int maxVectorSize = doubleArray[0].length;
+
+        for(int i = 1; i < doubleArray.length; i++){
+            if(maxVectorSize < doubleArray[i].length){
+                maxVectorSize = doubleArray[i].length;
+            }
+        }
+
+        this.m = maxVectorSize;
+        this.n = doubleArray.length;
+
+        vectors = new Vector[m];
+
+        for(int i = 0; i < doubleArray.length; i++){
+            vectors[i] = new Vector(m, doubleArray[i]);
+        }
+    }
+
+    public Matrix(Vector[] vector){
+        super(vector.length);
+        this.n = vector.length;
+        int maxVectorSize = vector[0].getSize();
+
+        for(int i = 1; i < vector.length; i++){
+            if(maxVectorSize < vector[i].getSize()){
+                maxVectorSize = vector[i].getSize();
+            }
+        }
+
+        this.m = maxVectorSize;
+        vectors = new Vector[n];
+
+        for(int i = 0; i < vector.length; i++){
+            vectors[i] = new Vector(m, vector[i].getComponentsArray());
+        }
+    }
+
+    public void print(){
+        for (Vector vector : vectors) {
+            System.out.println(vector);
+        }
+    }
+
+    public int getWidth(){
+        return m;
+    }
+
+    public int getHeight(){
+        return n;
+    }
+
+    public Vector getVector(int index){
+        return vectors[index];
+    }
+
+    public void setVector(int index, Vector vector){
+        vectors[index] = vector;
+    }
+
+    public Vector getColumnVector(int index){
+        double[] temp = new double[n];
+
+        for(int i = 0; i < n; i++){
+            temp[i] = vectors[i].getComponentsArray()[index];
+        }
+
+        return new Vector(temp);
+    }
+
+    public void transposition(){ // Ошибка в массиве, так как строки не равны
+        int maxVectorSize = vectors[0].getSize();
+
+        for(int i = 1; i < vectors.length; i++){
+            if(maxVectorSize < vectors[i].getSize()){
+                maxVectorSize = vectors[i].getSize();
+            }
+        }
+
+        Vector[] temp = new Vector[maxVectorSize];
+
+        for(int i = 0; i < maxVectorSize; i++){
+                temp[i] = this.getColumnVector(i);
+        }
+
+        vectors = temp;
+    }
+
+    public void makeMultiplication(double number){
+        for(int i = 0; i < n; i++){
+            vectors[i].makeMultiplication(number);
+        }
+    }
+
+    public int matrixDeterminant(){
+        return 0;
+    }
+
+    public String toString(){
+        StringBuilder stringBuilder = new StringBuilder("{");
+
+        for(int i = 0; i < n; i++){
+            stringBuilder.append(vectors[i].toString());
+
+            if(i != n - 1){
+                stringBuilder.append(", ");
+            }
+        }
+
+        stringBuilder.append('}');
+        return stringBuilder.toString();
+    }
 }
 
