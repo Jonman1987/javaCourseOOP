b88eb0e54f47716419077ff1c51fd19eab3f8dff
Jonman <Jonman@yandex.ru>
2024-03-05 23:35:04 +0700

    Task Csv - correction of mistakes v2.0. Date of last verification from Pavel 02.03.24

--- Csv/src/ru/academits/danilov_e/csv/ConverterFromCsvToHtml.java
+++ Csv/src/ru/academits/danilov_e/csv/ConverterFromCsvToHtml.java
@@ -5,46 +5,46 @@ import java.io.*;
 public class ConverterFromCsvToHtml {
     public static void convert(String inputFileName, String outputFileName) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
-             FileWriter fileWriter = new FileWriter(outputFileName, false)) {
-            printHeader(fileWriter);
-            printTable(fileWriter, reader);
-            printFooter(fileWriter);
+             PrintWriter printWriter = new PrintWriter(outputFileName)) {
+            printHeader(printWriter);
+            printTable(printWriter, reader);
+            printFooter(printWriter);
         }
     }
 
-    private static void printHeader(FileWriter fileWriter) throws IOException {
-        fileWriter.write("<!DOCTYPE html>\n");
-        fileWriter.write("<html lang=\"en\">\n");
-        fileWriter.write("\t<head>\n");
-        fileWriter.write("\t\t<meta charset=\"utf-8\">\n");
-        fileWriter.write("\t\t<title>Задача CSV</title>\n");
-        fileWriter.write("</head>\n");
-        fileWriter.write("\n");
-        fileWriter.write("<body>\n");
-        fileWriter.write("\t<table border = \"1\">\n");
+    private static void printHeader(PrintWriter printWriter) {
+        printWriter.println("<!DOCTYPE html>");
+        printWriter.println("<html lang=\"en\">");
+        printWriter.println("\t<head>");
+        printWriter.println("\t\t<meta charset=\"utf-8\">");
+        printWriter.println("\t\t<title>Задача CSV</title>");
+        printWriter.println("</head>");
+        printWriter.println();
+        printWriter.println("<body>");
+        printWriter.println("\t<table border = \"1\">");
     }
 
-    private static void printTable(FileWriter fileWriter, BufferedReader reader) throws IOException {
+    private static void printTable(PrintWriter printWriter, BufferedReader reader) throws IOException {
         String line;
-        boolean isTableStringOpen = false;
+        boolean isTableLineOpen = false;
         boolean isTableCellOpen = false;
         boolean isQuotesOpen = false;
 
         while ((line = reader.readLine()) != null) {
-            if (!isTableStringOpen) {
-                fileWriter.write("\t\t<tr>\n");
-                isTableStringOpen = true;
+            if (!isTableLineOpen) {
+                printWriter.println("\t\t<tr>");
+                isTableLineOpen = true;
             }
 
             for (int i = 0; i < line.length(); i++) {
                 if (!isTableCellOpen) {
-                    fileWriter.write("\t\t\t<td>");
+                    printWriter.print("\t\t\t<td>");
                     isTableCellOpen = true;
                 }
 
                 if (i != line.length() - 1) {
                     if (line.charAt(i) == '"' && isQuotesOpen && line.charAt(i + 1) == '"') {
-                        fileWriter.write('"');
+                        printWriter.print('"');
                         i++;
 
                         continue;
@@ -70,51 +70,51 @@ public class ConverterFromCsvToHtml {
                 }
 
                 if (line.charAt(i) == ',' && !isQuotesOpen) {
-                    fileWriter.write("</td>\n");
+                    printWriter.println("</td>");
                     isTableCellOpen = false;
 
                     if (i == line.length() - 1) {
-                        fileWriter.write("\t\t\t<td>");
+                        printWriter.print("\t\t\t<td>");
                     }
 
                     continue;
                 }
 
                 if (line.charAt(i) == '<') {
-                    fileWriter.write("&lt;");
+                    printWriter.print("&lt;");
 
                     continue;
                 }
 
                 if (line.charAt(i) == '>') {
-                    fileWriter.write("&gt;");
+                    printWriter.print("&gt;");
 
                     continue;
                 }
 
                 if (line.charAt(i) == '&') {
-                    fileWriter.write("&amp;");
+                    printWriter.print("&amp;");
 
                     continue;
                 }
 
-                fileWriter.write(line.charAt(i));
+                printWriter.write(line.charAt(i));
             }
 
             if (!isQuotesOpen) {
-                fileWriter.write("</td>\n");
+                printWriter.println("</td>");
                 isTableCellOpen = false;
-                fileWriter.write("\t\t</tr>\n");
-                isTableStringOpen = false;
+                printWriter.println("\t\t</tr>");
+                isTableLineOpen = false;
             } else {
-                fileWriter.write("<br/>");
+                printWriter.print("<br/>");
             }
         }
     }
 
-    private static void printFooter(FileWriter fileWriter) throws IOException {
-        fileWriter.write("\t</table>\n");
-        fileWriter.write("</body>\n");
-        fileWriter.write("</html>");
+    private static void printFooter(PrintWriter printWriter) {
+        printWriter.println("\t</table>");
+        printWriter.println("</body>");
+        printWriter.print("</html>");
     }
 }
--- Csv/src/ru/academits/danilov_e/csv_main/CsvMain.java
+++ Csv/src/ru/academits/danilov_e/csv_main/CsvMain.java
@@ -7,28 +7,44 @@ import java.io.IOException;
 
 public class CsvMain {
     private static String getOutputFileName(String inputFileName) {
+        int lastIndex = inputFileName.lastIndexOf('.');
+        inputFileName = inputFileName.substring(0, lastIndex + 1) + "html";
 
-        StringBuilder stringBuilder = new StringBuilder(inputFileName);
-
-        int dotIndex = stringBuilder.indexOf(".");
-        stringBuilder.delete(dotIndex + 1, stringBuilder.length());
-        stringBuilder.append("html");
-
-        return stringBuilder.toString();
+        return inputFileName;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Вы не указали файл для конвертации.");
+        } else if (args.length > 2) {
+            throw new IllegalArgumentException("Вы указали большое количество файлов. Укажите имя одного файла в " +
+                    "формате *.csv или двух файлов, где имя второго файла в формате *.html это файл результата конвертации.");
         } else {
             String fileName = args[0];
-            String outputFileName = getOutputFileName(fileName);
+            String outputFileName;
+
+            if (!fileName.substring(fileName.lastIndexOf('.') + 1).equalsIgnoreCase("csv")) {
+                throw new IllegalArgumentException("Вы указали файл не в формате *.csv.");
+            }
+
+            if (args.length == 2) {
+                fileName = args[0];
+                outputFileName = args[1];
+
+                if (!outputFileName.substring(outputFileName.lastIndexOf('.') + 1).equalsIgnoreCase("html")) {
+                    throw new IllegalArgumentException("Вы указали выходной файл не в формате *.html.");
+                }
+            } else {
+                outputFileName = getOutputFileName(fileName);
+            }
+
             try {
                 ConverterFromCsvToHtml.convert(fileName, outputFileName);
-            } catch (FileNotFoundException e){
-                throw new FileNotFoundException("Файл не найден");
+            } catch (FileNotFoundException e) {
+                System.out.println("Файл " + fileName + " не найден");
             } catch (IOException e) {
-                throw new IOException(e);
+                String error = e.toString();
+                System.out.println(error);
             }
         }
     }
