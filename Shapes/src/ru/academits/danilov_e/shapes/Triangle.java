a47792b0789ddd145614fe431f93726daea3e69a
Jonman <Jonman@yandex.ru>
2024-03-15 15:39:44 +0700

    Task Shapes - correction of mistakes v6.0. Date of last verification from Pavel 14.03.24

--- Shapes/src/ru/academits/danilov_e/shapes/Triangle.java
+++ Shapes/src/ru/academits/danilov_e/shapes/Triangle.java
@@ -115,7 +115,7 @@ public class Triangle implements Shape {
 
     @Override
     public double getArea() {
-        return ((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3)) / 2;
+        return Math.abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2;
     }
 
     private static double getSideLength(double x1, double y1, double x2, double y2) {
