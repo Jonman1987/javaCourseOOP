da8858c77ca169e1f024bcedaff15c527d298a82
Jonman <Jonman@yandex.ru>
2024-03-17 17:57:23 +0700

    Task HashTable work process 0.1

--- HashTable/src/ru/academits/danilov_e/hashtable/HashTable.java
+++ HashTable/src/ru/academits/danilov_e/hashtable/HashTable.java
@@ -2,16 +2,63 @@ package ru.academits.danilov_e.hashtable;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class HashTable<T> implements Collection<T> {
+    private LinkedList<T>[] items;
+    private int size;
+    public HashTable(T object){
+        if(object == null){
+            throw new NullPointerException("Added element is null");
+        }
+
+        items = (LinkedList<T>[]) new LinkedList[100];
+        size = 0;
+
+        add(object);
+    }
+
+    public HashTable(T object, int capacity){
+        if(capacity <= 0){
+            throw new IllegalArgumentException("Capacity must be greater than 0. Capacity is "
+                    + capacity + ".");
+        }
+
+        if(object == null){
+            throw new NullPointerException("Added element is null");
+        }
+
+        items = (LinkedList<T>[]) new LinkedList[capacity];
+        size = 0;
+
+        add(object);
+    }
+
+    public int tableLength(){ // Метод отладки используется для контроля размера массива. В дальнейшем можно удалить
+        return items.length;
+    }
+
+    public int tableIndex(T element){ // Метод отладки используется для контроля изменения индекса при изменении
+        // размера таблицы. В дальнейшем можно удалить
+        int index = -1;
+
+        for(int i = 0; i < items.length; i++){
+            if(items[i] != null && items[i].contains(element)){
+                index = i;
+            }
+        }
+
+        return index;
+    }
+
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size != 0;
     }
 
     @Override
@@ -26,7 +73,25 @@ public class HashTable<T> implements Collection<T> {
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        int elementsCount = 0;
+
+        for (LinkedList<T> item : items) {
+            if (item != null) {
+                elementsCount++;
+            }
+        }
+
+        Object[] objects = new Object[elementsCount];
+        int j = 0;
+
+        for (LinkedList<T> item : items) {
+            if (item != null) {
+                objects[j] = item;
+                j++;
+            }
+        }
+
+        return objects;
     }
 
     @Override
@@ -36,7 +101,34 @@ public class HashTable<T> implements Collection<T> {
 
     @Override
     public boolean add(T t) {
-        return false;
+        if(t == null){
+            throw new NullPointerException("Added element is null");
+        }
+
+        if(size == items.length){
+            LinkedList<T>[] itemsArray = (LinkedList<T>[]) new LinkedList[items.length * 2];
+
+            for(int i = 0; i < items.length; i++){
+                if(items[i] != null){
+
+                }
+            }
+        }
+
+        int index = Math.abs(t.hashCode() % items.length);
+        int oldSize = size;
+
+        if(items[index] == null){
+            LinkedList<T> element = new LinkedList<>();
+            element.add(t);
+            items[index] = element;
+            size++;
+        }else {
+            items[index].add(t);
+            size++;
+        }
+
+        return size != oldSize;
     }
 
     @Override
@@ -66,6 +158,7 @@ public class HashTable<T> implements Collection<T> {
 
     @Override
     public void clear() {
-
+        items = (LinkedList<T>[]) new LinkedList[100];
+        size = 0;
     }
 }
--- HashTable/src/ru/academits/danilov_e/hashtable_main/HashTableMain.java
+++ HashTable/src/ru/academits/danilov_e/hashtable_main/HashTableMain.java
@@ -1,7 +1,50 @@
 package ru.academits.danilov_e.hashtable_main;
 
+import ru.academits.danilov_e.hashtable.HashTable;
+
+import java.util.Arrays;
+
 public class HashTableMain {
     public static void main(String[] args) {
+        System.out.println("1. Создание Хеш-Таблицы с элементом \"Красный\":");
+        String color1 = "Красный";
+        HashTable<String> colorsTable1 = new HashTable<>(color1);
+        System.out.println("Количество элементов в таблице: " + colorsTable1.size());
+        System.out.println("Содержимое таблицы:");
+        System.out.println(Arrays.toString(colorsTable1.toArray()));
+        System.out.println("Размер таблицы: " + colorsTable1.tableLength());
+        System.out.println("Хэш элемента \"Красный\" с учетом размера таблицы: " + color1.hashCode() % colorsTable1.tableLength());
+        System.out.println("Позиция элемента \"Красный\" в таблице: " + colorsTable1.tableIndex(color1));
+        System.out.println();
 
+        int capacity1 = 50;
+        System.out.println("2. Создание Хеш-Таблицы с элементом \"Белый\" с размерностью " + capacity1 + ":");
+        String color2 = "Белый";
+        HashTable<String> colorsTable2 = new HashTable<>(color2, capacity1);
+        System.out.println("Количество элементов в таблице: " + colorsTable2.size());
+        System.out.println("Содержимое таблицы:");
+        System.out.println(Arrays.toString(colorsTable2.toArray()));
+        System.out.println("Размер таблицы: " + colorsTable2.tableLength());
+        System.out.println("Хэш элемента \"Белый\" с учетом размера таблицы: " + color2.hashCode() % colorsTable2.tableLength());
+        System.out.println("Позиция элемента \"Белый\" в таблице: " + colorsTable2.tableIndex(color2));
+        System.out.println();
+
+        System.out.println("3. Добавление элементов в таблицу:");
+
+        String color3 = "Черный";
+        String color4 = "Синий";
+        String color5 = "Зеленый";
+        String color6 = "Черный";
+
+        colorsTable1.add(color3);
+        colorsTable1.add(color4);
+        colorsTable1.add(color5);
+        colorsTable1.add(color6);
+
+        System.out.println("Количество элементов в таблице: " + colorsTable1.size());
+        System.out.println("Содержимое таблицы:");
+        System.out.println(Arrays.toString(colorsTable1.toArray()));
+        System.out.println("Размер таблицы: " + colorsTable1.tableLength());
+        System.out.println();
     }
 }
