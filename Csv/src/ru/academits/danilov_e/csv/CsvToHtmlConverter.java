f91fbfece77b6ab2538c1afe0b0d4c11bdcf25df
Jonman <Jonman@yandex.ru>
2024-03-23 22:53:30 +0700

    Task Csv - correction of mistakes v4.0. Date of last verification from Pavel 22.03.24

---
+++ Csv/src/ru/academits/danilov_e/csv/CsvToHtmlConverter.java
@@ -0,0 +1,109 @@
+package ru.academits.danilov_e.csv;
+
+import java.io.*;
+
+public class CsvToHtmlConverter {
+    public static void convert(String inputFileName, String outputFileName) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
+             PrintWriter writer = new PrintWriter(outputFileName)) {
+            printHeader(writer);
+            printTable(writer, reader);
+            printFooter(writer);
+        }
+    }
+
+    private static void printHeader(PrintWriter writer) {
+        writer.println("<!DOCTYPE html>");
+        writer.println("<html lang=\"en\">");
+        writer.println("<head>");
+        writer.println("\t<meta charset=\"utf-8\">");
+        writer.println("\t<title>Задача CSV</title>");
+        writer.println("</head>");
+        writer.println("<body>");
+        writer.println("\t<table border = \"1\">");
+    }
+
+    private static void printTable(PrintWriter writer, BufferedReader reader) throws IOException {
+        String line;
+        boolean isTableRowOpen = false;
+        boolean isTableCellOpen = false;
+        boolean isQuotesOpen = false;
+
+        while ((line = reader.readLine()) != null) {
+            if (!isTableRowOpen) {
+                writer.println("\t\t<tr>");
+                isTableRowOpen = true;
+            }
+
+            for (int i = 0; i < line.length(); i++) {
+                if (!isTableCellOpen) {
+                    writer.print("\t\t\t<td>");
+                    isTableCellOpen = true;
+                }
+
+                if (line.charAt(i) == '"') {
+                    if (i != line.length() - 1) {
+                        if (isQuotesOpen && line.charAt(i + 1) == '"') {
+                            writer.print('"');
+                            i++;
+                            continue;
+                        }
+
+                        if (line.charAt(i + 1) != '"') {
+                            isQuotesOpen = !isQuotesOpen;
+                            continue;
+                        }
+                    } else {
+                        isQuotesOpen = false;
+                        continue;
+                    }
+
+                    isQuotesOpen = !isQuotesOpen;
+                    continue;
+                }
+
+                if (line.charAt(i) == ',' && !isQuotesOpen) {
+                    writer.println("</td>");
+                    isTableCellOpen = false;
+
+                    if (i == line.length() - 1) {
+                        writer.print("\t\t\t<td>");
+                    }
+                    continue;
+                }
+
+                if (line.charAt(i) == '<') {
+                    writer.print("&lt;");
+                    continue;
+                }
+
+                if (line.charAt(i) == '>') {
+                    writer.print("&gt;");
+                    continue;
+                }
+
+                if (line.charAt(i) == '&') {
+                    writer.print("&amp;");
+                    continue;
+                }
+
+                writer.write(line.charAt(i));
+            }
+
+            if (!isQuotesOpen) {
+                writer.println("</td>");
+                isTableCellOpen = false;
+                writer.println("\t\t</tr>");
+                isTableRowOpen = false;
+            } else {
+                writer.print("<br/>");
+            }
+        }
+    }
+
+    private static void printFooter(PrintWriter writer) {
+        writer.println("\t</table>");
+        writer.println("</body>");
+        writer.print("</html>");
+    }
+}
--- Csv/src/ru/academits/danilov_e/csv_main/CsvMain.java
+++ Csv/src/ru/academits/danilov_e/csv_main/CsvMain.java
@@ -1,6 +1,6 @@
 package ru.academits.danilov_e.csv_main;
 
-import ru.academits.danilov_e.csv.ConverterFromCsvToHtml;
+import ru.academits.danilov_e.csv.CsvToHtmlConverter;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -9,20 +9,26 @@ public class CsvMain {
     private static String getOutputFileName(String inputFileName) {
         int lastIndex = inputFileName.lastIndexOf('.');
 
+        if(lastIndex == -1){
+            return inputFileName + ".html";
+        }
+
+        if(lastIndex == inputFileName.length() - 1){
+            return inputFileName + "html";
+        }
+
         return inputFileName.substring(0, lastIndex + 1) + "html";
     }
 
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Вы не указали файл для конвертации.");
-
             return;
         }
 
         if (args.length > 2) {
             System.out.println("Вы указали большое количество файлов. Укажите имя одного файла в " +
                     "формате *.csv или двух файлов, где имя второго файла в формате *.html это файл результата конвертации.");
-
             return;
         }
 
@@ -30,18 +36,13 @@ public class CsvMain {
         String outputFileName;
 
         if (args.length == 2) {
-            inputFileName = args[0];
             outputFileName = args[1];
-
-            if (!outputFileName.substring(outputFileName.lastIndexOf('.') + 1).equalsIgnoreCase("html")) {
-                System.out.println("Вы указали выходной файл не в формате *.html.");
-            }
         } else {
             outputFileName = getOutputFileName(inputFileName);
         }
 
         try {
-            ConverterFromCsvToHtml.convert(inputFileName, outputFileName);
+            CsvToHtmlConverter.convert(inputFileName, outputFileName);
         } catch (FileNotFoundException e) {
             System.out.println("Файл " + inputFileName + " не найден");
         } catch (IOException e) {
