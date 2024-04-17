9807c50a5664e66d91b1a746874743c1dba201dd
Jonman <Jonman@yandex.ru>
2024-04-17 23:33:13 +0700

    Task Minesweeper - work process 0.1.

---
+++ Minesweeper/src/ru/academits/danilov_e/MinesweeperMain.java
@@ -0,0 +1,7 @@
+package ru.academits.danilov_e;
+
+public class MinesweeperMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Minesweeper/src/ru/academits/danilov_e/minesweeper/Minesweeper.java
@@ -0,0 +1,4 @@
+package ru.academits.danilov_e.minesweeper;
+
+public class Minesweeper {
+}
