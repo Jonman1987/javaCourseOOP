acc77bf51f7a3064490e14fcf23392371b4397ef
Jonman <Jonman@yandex.ru>
2024-04-30 00:24:19 +0700

    Task Lambda - correction of mistakes v1.0. Date of last verification from Pavel 21.04.24

--- Lambda/src/ru/academits/danilov_e/lambda/Person.java
+++ Lambda/src/ru/academits/danilov_e/lambda/Person.java
@@ -1,58 +1,40 @@
 package ru.academits.danilov_e.lambda;
 
 public class Person {
-    String name;
-    int age;
+    private String name;
+    private int age;
 
-    public Person(String name, int age){
-        if(age < 0 || age > 130){
-            throw new IllegalArgumentException("Age must be in range [0; 130]. Current age is " + age + ".");
+    public Person(String name, int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Age must be greater than 0 or equal.");
         }
 
         this.name = name;
         this.age = age;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public int getAge(){
+    public int getAge() {
         return age;
     }
 
-    public void setAge(int age){
-        if(age < 0 || age > 130){
-            throw new IllegalArgumentException("Age must be in range [0; 130]. Current age is " + age + ".");
+    public void setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Age must be greater than 0 or equal.");
         }
 
         this.age = age;
     }
 
-    public int compare(Integer age) {
-        if(getAge() < age){
-            return -1;
-        }else if(getAge() > age){
-            return 1;
-        }
-
-        return 0;
-    }
-
     @Override
-    public boolean equals(Object obj) {
-        System.out.println("Data equals method");
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-
-        return this.age == ((Person) obj).age && this.name.equals(((Person) obj).name);
+    public String toString(){
+        return name + " " + age;
     }
 }
