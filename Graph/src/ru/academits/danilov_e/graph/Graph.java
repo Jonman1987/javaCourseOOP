dc57ff6cca92095dbf7074968d48648436a5ac00
Jonman <Jonman@yandex.ru>
2024-05-05 01:06:44 +0700

    Task Graph - correction of mistakes v1.1. Date of last verification from Pavel 28.04.24

--- Graph/src/ru/academits/danilov_e/graph/Graph.java
+++ Graph/src/ru/academits/danilov_e/graph/Graph.java
@@ -1,126 +1,109 @@
 package ru.academits.danilov_e.graph;
 
-import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.function.Consumer;
 
 public class Graph {
-    int[][] graph;
-    boolean[] hasVisited;
+    private final int[][] connectivityMatrix;
 
-    public Graph(int[][] graph) {
-        this.graph = graph;
-        hasVisited = new boolean[graph.length];
-
-        Arrays.fill(hasVisited, false);
-    }
-
-    public void showVisitedNodes() {
-        for (boolean node : hasVisited) {
-            System.out.println(node);
+    public Graph(int[][] connectivityMatrix) {
+        if (connectivityMatrix.length != connectivityMatrix[0].length) {
+            throw new IllegalArgumentException("Connectivity matrix must have same count of rows and columns");
         }
+
+        this.connectivityMatrix = connectivityMatrix;
     }
 
-    public int checkVisitedNodes() {
-        for (int i = 0; i < hasVisited.length; i++) {
-            if (!hasVisited[i]) {
-                return i;
+    public void widthVisit(int startVertex, Consumer<Integer> consumer) {
+        boolean[] visited = new boolean[connectivityMatrix.length];
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(startVertex);
+        Integer vertex;
+
+        while (!queue.isEmpty()) {
+            vertex = queue.remove();
+            visited[vertex] = true;
+
+            consumer.accept(vertex);
+
+            for (int i = 0; i < connectivityMatrix.length; i++) {
+                if (connectivityMatrix[vertex][i] != 0 && !visited[i]) {
+                    queue.add(i);
+                }
             }
         }
-
-        return -1;
     }
 
-    public void startVisit(String algorithmType) {
-        switch (algorithmType) {
-            case "Width":
+    public void depthVisit(int startVertex, Consumer<Integer> consumer) {
+        boolean[] visited = new boolean[connectivityMatrix.length];
 
-                while (checkVisitedNodes() != -1) {
-                    visitInWidth(checkVisitedNodes());
+        Stack<Integer> list = new Stack<>();
+
+        list.addFirst(startVertex);
+        Integer vertex;
+
+        while (!list.isEmpty()) {
+            vertex = list.removeFirst();
+            visited[vertex] = true;
+
+            consumer.accept(vertex);
+
+            for (int i = 0; i < connectivityMatrix.length; i++) {
+                if (connectivityMatrix[vertex][i] != 0 && !visited[i]) {
+                    list.add(i);
                 }
-
-                break;
-            case "Deep":
-
-                while (checkVisitedNodes() != -1) {
-                    visitInDepth(checkVisitedNodes());
-                }
-
-                break;
-            default:
-                throw new IllegalArgumentException("Выбран не верный вид обхода графа. Возможные варианты Width " +
-                        "или Deep. Ваш вариант: " + algorithmType);
-        }
-    }
-
-    private LinkedList<Integer> getNearestNodesList(int nodes) {
-        LinkedList<Integer> nodesList = new LinkedList<>();
-
-        int[] nodesLine = graph[nodes];
-
-        for (int i = 0; i < nodesLine.length; i++) {
-            if (nodesLine[i] != 0 && i != nodes && !hasVisited[i]) {
-                nodesList.addLast(i);
             }
         }
-
-        return nodesList;
     }
 
-    private void visitInWidth(int startNode) {
-        System.out.println(startNode);
-        hasVisited[startNode] = true;
-        LinkedList<Integer> nodesList = new LinkedList<>(getNearestNodesList(startNode));
+    public <T> void recursivelyDepthVisit(int startVertex, Consumer<Integer> consumer) {
+        boolean[] visited = new boolean[connectivityMatrix.length];
+        Stack<Integer> list = new Stack<>();
+        list.add(startVertex);
+        depthVisit(list, visited, consumer);
+    }
 
-        while (!nodesList.isEmpty()) {
-            int node = nodesList.getFirst();
-            System.out.println(node);
-            hasVisited[node] = true;
-            nodesList.removeFirst();
-            nodesList.addAll(getNearestNodesList(node));
+    private void depthVisit(Stack<Integer> list, boolean[] array, Consumer<Integer> consumer) {
+        while (!list.isEmpty()) {
+            int vertex = list.removeFirst();
+            consumer.accept(vertex);
+            array[vertex] = true;
+
+            for (int i = 0; i < connectivityMatrix.length; i++) {
+                if (connectivityMatrix[vertex][i] != 0 && !array[i]) {
+                    list.add(i);
+                }
+            }
+
+            depthVisit(list, array, consumer);
         }
     }
 
-    private void visitInDepth(int startNode) {
-        System.out.println(startNode);
-        hasVisited[startNode] = true;
-        LinkedList<Integer> childList = getNearestNodesList(startNode);
-        childList.reversed();
-        LinkedList<Integer> nodesList = new LinkedList<>(childList);
-
-        while (!nodesList.isEmpty()) {
-            int node = nodesList.getFirst();
-            System.out.println(node);
-            hasVisited[node] = true;
-            nodesList.removeFirst();
-            nodesList.addAll(getNearestNodesList(node).reversed());
-        }
+    public void recursivelyWidthVisit(int startVertex, Consumer<Integer> consumer) {
+        boolean[] visited = new boolean[connectivityMatrix.length];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(startVertex);
+        widthVisit(queue, visited, consumer);
     }
 
-    public void clearVisitedNodes() {
-        Arrays.fill(hasVisited, false);
+    private void widthVisit(Queue<Integer> queue, boolean[] array, Consumer<Integer> consumer) {
+        while (!queue.isEmpty()) {
+            int vertex = queue.remove();
+            array[vertex] = true;
+
+            consumer.accept(vertex);
+
+            for (int i = 0; i < connectivityMatrix.length; i++) {
+                if (connectivityMatrix[vertex][i] != 0 && !array[i]) {
+                    queue.add(i);
+                }
+            }
+
+            widthVisit(queue, array, consumer);
+        }
     }
-
-    /*public void widthTreeShowWithRecursion() {
-        LinkedList<TreeNode<E>> list = new LinkedList<>();
-        list.add(root);
-        widthVisit(list);
-    }
-
-    private void widthVisit(LinkedList<TreeNode<E>> list) {
-        System.out.println(list.getFirst());
-
-        if (list.getFirst().getLeftChild() != null) {
-            list.addLast(list.getFirst().getLeftChild());
-        }
-
-        if (list.getFirst().getRightChild() != null) {
-            list.addLast(list.getFirst().getRightChild());
-        }
-
-        list.removeFirst();
-
-        if (!list.isEmpty()) {
-            widthVisit(list);
-        }
-    }*/
 }
--- Graph/src/ru/academits/danilov_e/graph_main/GraphMain.java
+++ Graph/src/ru/academits/danilov_e/graph_main/GraphMain.java
@@ -2,6 +2,8 @@ package ru.academits.danilov_e.graph_main;
 
 import ru.academits.danilov_e.graph.Graph;
 
+import java.util.function.Consumer;
+
 public class GraphMain {
     public static void main(String[] args) {
         int[][] array = {
@@ -16,29 +18,53 @@ public class GraphMain {
 
         Graph graph = new Graph(array);
 
-        System.out.println("Массив посещенных вершин:");
-        graph.showVisitedNodes();
-        System.out.println();
+        Consumer<Integer> printer = x -> System.out.printf("%d ", x);
 
         System.out.println("Проход по графу в глубину:");
-        graph.startVisit("Deep");
+        System.out.println("Первая компонента:");
+        graph.depthVisit(0, printer);
         System.out.println();
-
-        System.out.println("Массив посещенных вершин:");
-        graph.showVisitedNodes();
+        System.out.println("Вторая компонента:");
+        graph.depthVisit(4, printer);
         System.out.println();
-
-        graph.clearVisitedNodes();
-
-        System.out.println("Массив посещенных вершин:");
-        graph.showVisitedNodes();
+        System.out.println("Третья компонента:");
+        graph.depthVisit(5, printer);
+        System.out.println();
         System.out.println();
 
         System.out.println("Проход по графу в ширину:");
-        graph.startVisit("Width");
+        System.out.println("Первая компонента:");
+        graph.widthVisit(0, printer);
+        System.out.println();
+        System.out.println("Вторая компонента:");
+        graph.widthVisit(4, printer);
+        System.out.println();
+        System.out.println("Третья компонента:");
+        graph.widthVisit(5, printer);
+        System.out.println();
         System.out.println();
 
-        System.out.println("Массив посещенных вершин:");
-        graph.showVisitedNodes();
+        System.out.println("Рекурсивный проход по графу в глубину:");
+        System.out.println("Первая компонента:");
+        graph.recursivelyDepthVisit(0, printer);
+        System.out.println();
+        System.out.println("Вторая компонента:");
+        graph.recursivelyDepthVisit(4, printer);
+        System.out.println();
+        System.out.println("Третья компонента:");
+        graph.recursivelyDepthVisit(5, printer);
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Рекурсивный проход по графу в ширину:");
+        System.out.println("Первая компонента:");
+        graph.recursivelyWidthVisit(0, printer);
+        System.out.println();
+        System.out.println("Вторая компонента:");
+        graph.recursivelyWidthVisit(4, printer);
+        System.out.println();
+        System.out.println("Третья компонента:");
+        graph.recursivelyWidthVisit(5, printer);
+        System.out.println();
     }
 }
