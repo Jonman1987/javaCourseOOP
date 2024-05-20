00dcd63b1eeba7f5a2f9682cc2e85db4ea462f6b
Jonman <Jonman@yandex.ru>
2024-05-20 23:44:38 +0700

    test

--- Draft/src/Main.java
+++ Draft/src/Main.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+        int x = 5 * 4 % 3;
+        System.out.println(x);
     }
 }
